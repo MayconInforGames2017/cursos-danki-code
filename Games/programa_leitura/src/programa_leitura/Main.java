70c12c38ce7361d159969076409b17ac0bfb8bb5
MayconInforgames <maycon_gus2009@hotmail.com>
2021-03-11 20:58:38 -0300

    programa de leitura

---
+++ Games/programa_leitura/src/programa_leitura/Main.java
@@ -0,0 +1,14 @@
+package programa_leitura;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite o seu nome: ");
+		String nome = scan.nextLine();
+
+	}
+
+}
