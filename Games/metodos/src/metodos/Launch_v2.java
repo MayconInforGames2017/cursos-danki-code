29c781ef72c6852d576cbaf333321e37c420a8e6
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-05-21 20:55:46 -0300

    usando o metodo static

---
+++ Games/metodos/src/metodos/Launch_v2.java
@@ -0,0 +1,16 @@
+package metodos;
+
+public class Launch_v2 {
+	
+	public void exemplo() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Launch_v2 launch_v2 = new Launch_v2();
+		launch_v2.exemplo();
+
+	}
+
+}
